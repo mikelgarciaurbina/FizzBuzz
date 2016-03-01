48dfba463ae2a283ab343691b19a22ecc4763ca4
Mikel Garcia <mikel_6@hotmail.com>
2016-03-01 15:37:50 +0100

    add condition starts with the number 1

--- fizzbuzz-each.rb
+++ fizzbuzz-each.rb
@@ -1,11 +1,16 @@
 (1..100).each do |i|
-	if i % 15 == 0
-		puts "FizzBuzz"
-	elsif i % 3 == 0
-		puts "Fizz"
-	elsif i % 5 == 0
-		puts "Buzz"
-	else
-		puts i
+	result = ""
+	if i % 3 == 0
+		result << "Fizz"
 	end
+	if i % 5 == 0
+		result << "Buzz"
+	end
+	if i.to_s[0].to_i == 1
+		result << "Bang"
+	end
+	if result == ""
+		result << i.to_s
+	end
+	puts result
 end
